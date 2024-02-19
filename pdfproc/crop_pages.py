12a99049235ce16ad95e8cc1d75d42dad6a4d019
myfeing <55974549+myfeing@users.noreply.github.com>
2024-02-19 20:03:24 +0800

    ;;

--- pdfproc/crop_pages.py
+++ pdfproc/crop_pages.py
@@ -2,27 +2,27 @@ import fitz
 
 
 def run():
-    pf = r"C:\Users\Lenovo\Downloads\books\西安事變新探 (杨奎松).pdf"
+    pf = r"C:\Users\myfei\Downloads\Goldstein, Rebecca - Incompleteness (Godel).pdf"
     doc = fitz.open(pf)
+    of = r"C:\Users\myfei\Downloads\Goldstein, Rebecca - Incompleteness (Godel)2.pdf"
+    out = fitz.open()
     ori_cnt = doc.page_count
     for i in range(doc.page_count):
         page = doc[i]
-        if i == 0:
+        if i == -1:
             doc.fullcopy_page(i)
         else:
-            rect = page.rect
-            rect2 = fitz.Rect(rect.x0, rect.y0, rect.y1, round(rect.x1/2, 3))
-            print(rect, rect2, sep=' ')
-            page.set_cropbox(rect2)
-            doc.fullcopy_page(i)
+            #rect = page.rect
+            #rect2 = fitz.paper_rect('A5')
+            page.set_cropbox(fitz.Rect(144, 32, 462, 624))
+            page2 = out.new_page() #(width=rect2.width, height=rect2.height)
+            page2.show_pdf_page(page2.rect, doc, i, keep_proportion=False)
             # page.set_mediabox(page.mediabox)
-            rect3 = fitz.Rect(round(rect.y1/2, 3), rect.y0, rect.y1, rect.x1)
-            page.set_cropbox(rect3)
-            doc.fullcopy_page(i)
+            #rect3 = fitz.Rect(round(rect.y1/2, 3), rect.y0, rect.y1, rect.x1)
+            #doc.fullcopy_page(i)
 
-    doc.delete_pages(0, ori_cnt)
-    of = r"C:\Users\Lenovo\Downloads\books\西安事變新探 (杨奎松)2.pdf"
-    doc.save(of)
+    #doc.delete_pages(0, ori_cnt)
+    out.save(of)
 
 
 if __name__ == '__main__':
