6afe11e6bbb915395f2134f5a4d1cf4e01b67218
myfeing <55974549+myfeing@users.noreply.github.com>
2024-04-12 20:42:08 +0800

    ；

--- pdfproc/crop_pages.py
+++ pdfproc/crop_pages.py
@@ -2,9 +2,9 @@ import fitz
 
 
 def run():
-    pf = r"C:\Users\myfei\Downloads\Goldstein, Rebecca - Incompleteness (Godel).pdf"
+    pf = r"C:\Users\myfei\Downloads\Incompleteness - The Proof and Paradox of Kurt Godel by Rebecca Goldstein（2005）.pdf"
     doc = fitz.open(pf)
-    of = r"C:\Users\myfei\Downloads\Goldstein, Rebecca - Incompleteness (Godel)2.pdf"
+    of = r"C:\Users\myfei\Downloads\Incompleteness - The Proof and Paradox of Kurt Godel by Rebecca Goldstein（2005）2.pdf"
     out = fitz.open()
     ori_cnt = doc.page_count
     for i in range(doc.page_count):
@@ -16,7 +16,7 @@ def run():
             #rect2 = fitz.paper_rect('A5')
             page.set_cropbox(fitz.Rect(144, 32, 462, 624))
             page2 = out.new_page() #(width=rect2.width, height=rect2.height)
-            page2.show_pdf_page(page2.rect, doc, i, keep_proportion=False)
+            page2.show_pdf_page(page2.rect, doc, i, keep_proportion=True)
             # page.set_mediabox(page.mediabox)
             #rect3 = fitz.Rect(round(rect.y1/2, 3), rect.y0, rect.y1, rect.x1)
             #doc.fullcopy_page(i)
--- pdfproc/delete_page.py
+++ pdfproc/delete_page.py
@@ -1,9 +1,9 @@
 import fitz
 
-path = r"C:\Users\myfei\proj\python\\"
+path = r"C:\Users\myfei\Downloads\\"
 
 # Path of the PDF file
-input_file = path + r'Foundations of Image Science by Harrison H. Barrett, Kyle J. Myers.pdf'
+input_file = path + r'Incompleteness - The Proof and Paradox of Kurt Godel by Rebecca Goldstein（2005）2.pdf'
 
 # Path for the output PDF file
 # output_file = path + r"XML Processing with Python2.pdf"
@@ -16,7 +16,7 @@ file_handle = fitz.open(input_file)
 # pages_list = [5]
 
 # Passing the list to the select function
-file_handle.delete_page(1558)
+file_handle.delete_page(0)
 
 # Saving the file
 file_handle.saveIncr()
