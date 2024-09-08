e1f83b3c72c6727a8ba21edeb25de53ef8bcd5e4
myfei <55974549+myfeing@users.noreply.github.com>
2024-09-08 19:11:22 +0800

    ;;

--- qt/internetarchive2cue/translations/internetarchive2cue_zh_CN.ts
+++ qt/internetarchive2cue/translations/internetarchive2cue_zh_CN.ts
@@ -9,14 +9,24 @@
         <translation>互联网档案馆音频分段数据文件转换为cue文件</translation>
     </message>
     <message>
-        <location filename="../form.ui" line="29"/>
+        <location filename="../form.ui" line="32"/>
         <source>Audio Segment Data File</source>
         <translation>音频分段数据文件</translation>
     </message>
     <message>
-        <location filename="../form.ui" line="39"/>
+        <location filename="../form.ui" line="42"/>
         <source>...</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../form.ui" line="58"/>
+        <source>Convert</source>
+        <translation>转换</translation>
+    </message>
+    <message>
+        <location filename="../form.ui" line="71"/>
+        <source>Close</source>
+        <translation>关闭</translation>
     </message>
 </context>
 </TS>
